4d782d6f9ff2622ea02bcef6bff7673a9a9763f4
taso <chlxogns92@postech.ac.kr>
2017-04-10 17:20:40 +0900

    Modify direct_cycle_executor.sh

--- advanced/direct_cycle_executor.sh
+++ advanced/direct_cycle_executor.sh
@@ -2,4 +2,6 @@
 k=3
 while [ $k -lt 16 ]; do python direct_cycle.py $k >> ../results/direct_cycle_result.txt; k=$((k+1)); done
 k=20; python direct_cycle.py $k >> ../results/direct_cycle_result.txt
+k=25; python direct_cycle.py $k >> ../results/direct_cycle_result.txt
 k=30; python direct_cycle.py $k >> ../results/direct_cycle_result.txt
+k=50; python direct_cycle.py $k >> ../results/direct_cycle_result.txt
