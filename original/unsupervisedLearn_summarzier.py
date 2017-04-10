9d5428c4415820ecfa45d86607117a7f826837bc
taso <chlxogns92@gmail.com>
2017-04-10 17:40:51 +0900

    Implement unsupervisedLearn_summarzier.py

---
+++ original/unsupervisedLearn_summarzier.py
@@ -0,0 +1,16 @@
+f=open('../results/unsupervisedLearn_result.txt')
+contents=f.read()
+parts=contents.split('k:')[1:]
+summaries=[]
+for part in parts:
+	lines=part.split('\n')
+	summary={'k':int(lines[0])}
+	for line in lines:
+		if line.find('precision') != -1:
+			summary['precision']=float(line.split(':')[1])
+		if line.find('recall') != -1:
+			summary['recall']=float(line.split(':')[1])
+	summaries.append(summary)
+
+import json
+print json.dumps(summaries)
