6e9c11885f56b8067b161d4cce8b9d115e400baa
taso <chlxogns92@postech.ac.kr>
2017-04-10 23:10:43 +0900

    Move summarizers and executors to tools folder

--- advanced/direct_cycle_executor.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-k=3
-while [ $k -lt 16 ]; do python direct_cycle.py $k >> ../results/direct_cycle_result.txt; k=$((k+1)); done
-k=20; python direct_cycle.py $k >> ../results/direct_cycle_result.txt
-k=25; python direct_cycle.py $k >> ../results/direct_cycle_result.txt
-k=30; python direct_cycle.py $k >> ../results/direct_cycle_result.txt
-k=50; python direct_cycle.py $k >> ../results/direct_cycle_result.txt
--- original/unsupervisedLearn_executor.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-k=3
-while [ $k -lt 16 ]; do python unsupervisedLearn.py $k >> ../results/unsupervisedLearn_result.txt; k=$((k+1)); done
-k=20; python unsupervisedLearn.py $k >> ../results/unsupervisedLearn_result.txt
-k=25; python unsupervisedLearn.py $k >> ../results/unsupervisedLearn_result.txt
-k=30; python unsupervisedLearn.py $k >> ../results/unsupervisedLearn_result.txt
-k=50; python unsupervisedLearn.py $k >> ../results/unsupervisedLearn_result.txt
-
